24edbeb42ac08dab8a17f649d39ecb5a8fae0056
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-12-18 15:31:27 -0300

    ahora si lista para el edploy

--- application/APIREST/SummaryApi.py
+++ application/APIREST/SummaryApi.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from application.service.SummaryService import SummaryService
 from decouple import config  
 from dotenv import load_dotenv 
-
+import os 
 
 load_dotenv() # para cargar el .env en local
 
@@ -16,11 +16,9 @@ app = Flask(__name__)
 @app.route("/resumen", methods=["POST"])
 def recibir_mensaje():
     incoming_message = request.form.get("Body").strip().lower()  # Mensaje de WhatsApp
-    sender = request.form.get("From")  # Número del remitente
-
+    
     if incoming_message == "resumen":
         try:
-            # Llama a tu lógica
             url = "https://game.systemmaster.com.ar/frmLogin.aspx"
             user = config("DB_USER")
             password = config("DB_PASSWORD")
@@ -38,5 +36,26 @@ def recibir_mensaje():
                    </Response>"""
     return response, 200, {'Content-Type': 'application/xml'}
 
+
+@app.route("/obtenerResumen", methods=["GET"])
+def obtener_resumen():
+    url = "https://game.systemmaster.com.ar/frmLogin.aspx"
+    user = config("DB_USER")
+    password = config("DB_PASSWORD")
+    
+    try:
+        # Obtener el resumen usando el servicio
+        total = SummaryService.get_summary(url, user, password)
+        response_message = {"message": f"El total es: {total}"}
+        return jsonify(response_message), 200
+
+    except Exception as e:
+        # Manejo de errores
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 500
+           
+
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto como variable de entorno
+    app.run(host="0.0.0.0", port=port, debug=True)
