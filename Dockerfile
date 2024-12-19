aa0cfedc45e0800f5e8396624e5f813d3eae9edb
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-12-18 23:49:08 -0300

    cree el Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Define una variable de argumento para el puerto (puedes cambiarlo al build si es necesario)
+ARG PORT=443
+
+# Usa una imagen base con soporte de Python (no es necesario usar una imagen de Cypress si no usas sus funcionalidades)
+FROM python:3.10-slim-buster
+
+# Establece un directorio de trabajo dentro del contenedor
+WORKDIR /app
+
+# Copia el archivo de dependencias primero para aprovechar la cache en las instalaciones
+COPY requirements.txt ./requirements.txt
+
+# Instala las dependencias necesarias del sistema y Python
+RUN apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    && pip install --no-cache-dir -r requirements.txt
+                                     
+# Copia el resto de los archivos del proyecto al directorio de trabajo
+COPY . .
+
+# Define la variable de entorno para el PATH
+ENV PATH="/root/.local/bin:${PATH}"
+
+# Expone el puerto para que esté disponible externamente
+EXPOSE $PORT
+
+# Comando para ejecutar la aplicación
+CMD ["uvicorn", "application.APIREST.SummaryApi:app", "--host", "0.0.0.0", "--port", "443"]
--- application/automation/WebDriverManager.py
+++ application/automation/WebDriverManager.py
@@ -10,6 +10,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
+import os 
+
 
 class WebDriverManager:
 
@@ -19,21 +21,6 @@ class WebDriverManager:
         self.configure_driver()
         self.expected_filename_pattern = expected_filename_pattern
         self.file_downloaded_path = None
-
-    def configure_driver(self):
-        service = Service(ChromeDriverManager().install())
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        # options.add_argument("--window-size=1920,1080") # si queres ver el proceso, descomenta esta linea y comenta options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_experimental_option("prefs", {
-            "download.default_directory": self.output_path,
-            "download.prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "safebrowsing.enabled": True
-        })
-        self.driver = Chrome(service=service, options=options)
     
     def start(self, url, user, password):
         self.driver.get(url)
@@ -76,3 +63,26 @@ class WebDriverManager:
     def quit_driver(self):
         if self.driver:
             self.driver.quit()
+
+    def configure_driver(self):
+        # Utilizamos ChromeDriverManager para manejar la instalación del driver
+        service = Service(ChromeDriverManager().install())
+        
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        
+        # Verifica si se puede acceder al binario de google-chrome-stable y configura la ubicación
+        if os.path.exists("/usr/bin/google-chrome-stable"):
+            options.binary_location = "/usr/bin/google-chrome-stable"
+        
+        options.add_experimental_option("prefs", {
+            "download.default_directory": self.output_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        })
+        
+        # Crea el WebDriver con la configuración
+        self.driver = webdriver.Chrome(service=service, options=options)
