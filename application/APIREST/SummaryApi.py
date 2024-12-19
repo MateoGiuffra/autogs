a51ca4fd7d647154e535b9e4b7973a5c06c6ca0f
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-12-19 19:12:53 -0300

    agrego cache

--- application/APIREST/SummaryApi.py
+++ application/APIREST/SummaryApi.py
@@ -1,62 +1,79 @@
 from flask import Flask, jsonify, request
 from application.service.SummaryService import SummaryService
 from decouple import config  
-from dotenv import load_dotenv 
 import os 
-
+import time 
+from dotenv import load_dotenv 
 load_dotenv()  # para cargar el .env en local
 
-app = Flask(__name__)
+class SummaryApi:
+    BASE_URL = "https://game.systemmaster.com.ar/frmLogin.aspx"
+    DB_USER = config("DB_USER")
+    DB_PASSWORD = config("DB_PASSWORD")
+    CACHE_TIMEOUT = 10 * 60
+    TOTAL = 0
+    cache = {
+    "summary": None, 
+    "timestamp":0 
+    }
 
-@app.route("/resumen", methods=["POST"])
-def recibir_mensaje():
-    # Obtener el mensaje del cuerpo de la solicitud
-    incoming_message = request.form.get("Body").strip().lower()  # Mensaje de WhatsApp
-    print("Mensaje recibido:", incoming_message)  # Ver lo que llega
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.total = 0
+        self.setup_routes()
+
+    def setup_routes(self):
+        @self.app.route("/obtenerResumen", methods=["GET"])
+        def obtener_resumen():
+            try:
+                return jsonify(self.get_summary()), 200
+            except Exception as e:
+                error_message = {"error": str(e)}
+                return jsonify(error_message), 500
+        
+        @self.app.route("/resumen", methods=["POST"])
+        def recibir_mensaje():
+            incoming_message = request.form.get("Body", "").strip().lower()
+            try:
+                self.validate_incoming_message(incoming_message)
+                response_message = self.get_summary()
+                return self.answer_message(response_message, 200)
+            except ValueError as ve:
+                response_message = self.answer_message(str(ve))
+                return self.answer_message(response_message, 400)
     
-    if incoming_message == "resumen":
-        print("Entrando en el bloque de resumen")  # Ver si entra aquí
-        try:
-            url = "https://game.systemmaster.com.ar/frmLogin.aspx"
-            user = config("DB_USER")
-            password = config("DB_PASSWORD")
-            
-            # Llamada al servicio para obtener el resumen
-            total = SummaryService.get_summary(url, user, password)
-            response_message = f"El total es: {total}"
-        except Exception as e:
-            response_message = f"Error: {str(e)}"
-            print("Error al obtener el resumen:", e)  # Ver el error si ocurre
-    else:
-        print("Mensaje no reconocido:", incoming_message)  # Ver el mensaje no reconocido
-        response_message = "Mensaje no reconocido. Envía 'resumen' para obtener el total."
+    def validate_incoming_message(self, incoming_message):
+        if (incoming_message != "resumen"): 
+             raise ValueError("Mensaje no reconocido. Envía 'resumen' para obtener el total.")
 
-    # Respuesta al remitente
-    response = f"""<Response>
-                      <Message>{response_message}</Message>
-                   </Response>"""
-    return response, 200, {'Content-Type': 'application/xml'}
-
-
-@app.route("/obtenerResumen", methods=["GET"])
-def obtener_resumen():
-    url = "https://game.systemmaster.com.ar/frmLogin.aspx"
-    user = config("DB_USER")
-    password = config("DB_PASSWORD")
+    def get_summary(self):
+        try: 
+            self.set_new_total()           
+            return  f"El total es: {SummaryApi.total}"
+        except Exception as e: 
+            return f"Error: {str(e)}"
     
-    try:
-        # Obtener el resumen usando el servicio
-        total = SummaryService.get_summary(url, user, password)
-        response_message = {"message": f"El total es: {total}"}
-        return jsonify(response_message), 200
+    def set_new_total(self):
+        current_time = time.time()
+        time_since_last_summary = current_time - SummaryApi.cache["timestamp"]
+        # si fue pedido hace menos de 10 minutos no hace nada
+        if SummaryApi.cache["summary"] is not None and  time_since_last_summary < SummaryApi.CACHE_TIMEOUT: return
+        # caso contrario, calcula
+        SummaryApi.total = SummaryService.get_summary(SummaryApi.BASE_URL, SummaryApi.DB_USER, SummaryApi.DB_PASSWORD)
+        SummaryApi.cache["summary"] =  SummaryApi.total 
+        SummaryApi.cache["timestamp"] = current_time 
+    
+    def answer_message(self, message, value):
+        response = f"""<Response>
+                         <Message>{message}</Message>
+                    </Response>"""
+        return response, value, {'Content-Type': 'application/xml'}   
 
-    except Exception as e:
-        # Manejo de errores
-        error_message = {"error": str(e)}
-        return jsonify(error_message), 500
-           
+    def run(self):
+        port = int(os.environ.get("PORT", 10000))
+        self.app.run(host="0.0.0.0", port=port, debug=True)
 
-
+# Ejecutar la aplicación
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto como variable de entorno
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app_routes = SummaryApi()
+    app_routes.run()
