251903cb39a57a4bf115af87b09b012e86e90ec1
MateoGiuffra <matteogiuffrah40@gmail.com>
2025-01-10 00:12:04 -0300

    agrego monitoreo de memoria

--- application/controller/APIREST/wsgi.py
+++ application/controller/APIREST/wsgi.py
@@ -1,5 +1,9 @@
 from application.controller.APIREST.SummaryApi import SummaryApi
-
+import psutil
+import os 
 # Instanciar la aplicación
 app_routes = SummaryApi()
 app = app_routes.app  
+
+process = psutil.Process(os.getpid())
+print(f"Memory usage after SummaryApi instantiation: {process.memory_info().rss / 1024 ** 2} MB")
