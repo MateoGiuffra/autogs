d6f8c3c6d6e1c02fd52557bae0cecdaf4dd79ce9
MateoGiuffra <matteogiuffrah40@gmail.com>
2025-02-26 16:07:40 -0300

    agrego diseño responsive

--- application/persistence/SummaryDAO.py
+++ application/persistence/SummaryDAO.py
@@ -21,15 +21,12 @@ class SummaryDAO:
     def update_summary(self, summary):
         try:
             doc_ref = self.db.collection("summary").document(f"{summary.get_month_and_year()}")
-            # actual_more_three_hours = summary.get_last_report_date()  + timedelta(hours=3)
-            # in_iso_format = actual_more_three_hours.isoformat()
-            in_iso_format = summary.get_last_report_date().isoformat()
             doc_ref.update({
                 "total": summary.get_total(),
                 "last_total": summary.get_last_total(),
                 "last_months_total": summary.get_last_months_total(),
                 "last_months_total_today": summary.get_last_months_total_today(), 
-                "last_report_date": in_iso_format,
+                "last_report_date": summary.get_last_report_date().isoformat(),
                 "date_of_lmtt": summary.get_date_of_lmtt(),
                 "date_of_lmt": summary.get_date_of_lmt()
             })
